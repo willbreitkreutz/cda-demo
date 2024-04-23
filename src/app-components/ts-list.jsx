cd5e0fb8dcd30dbfb86ef7a7a4a59522006dd4cc
Breitkreutz <1274303081121002@mil>
2024-04-23 16:16:31 -0500

    add find max

--- src/app-components/ts-list.jsx
+++ src/app-components/ts-list.jsx
@@ -1,9 +1,16 @@
 import { useState, useEffect } from "react";
 import TsListItem from "./ts-list-item";
 
+function findMax(values){
+    const sorted = values.map((val) => {
+        return val[1]
+    }).filter((val) => { return val !== null}).sort();
+    return sorted[sorted.length-1]
+}
 
 function TsList({ district, tsId }) {
   const [values, setValues] = useState([]);
+  const [maxValue, setMaxValue] = useState(0)
 
   useEffect(() => {
     const url = `https://cwms-data.usace.army.mil/cwms-data/timeseries?name=${tsId}&office=${district}`
@@ -19,15 +26,19 @@ function TsList({ district, tsId }) {
         }
     }).then((data) => {
         setValues(data?.values || [])
+        setMaxValue(findMax(data?.values || []))
     })
   }, [district, tsId])
 
   return (
+    <>
+    <h2>{maxValue}</h2>
     <ul>
       {values.map((val) => {
         return <TsListItem key={val[0]} time={val[0]} val={val[1]} />;
       })}
     </ul>
+    </>
   );
 }
 
